a89c97d908b749f9a0fdb8d99e1273a0989c5c45
Nikhil Puri Goswami <nikhilpurigoswami20@gmail.com>
2026-06-22 13:44:35 +0530

    Added

---
+++ Patterns/pascalTriangle.cpp
@@ -0,0 +1,54 @@
+// #include <iostream>
+// using namespace std;
+
+// int main() {
+//     int n;
+//     cout << "Enter number of rows: ";
+//     cin >> n;
+
+//     for(int i = 0; i < n; i++) {
+
+//         int val = 1;
+
+//         for(int j = 0; j <= i; j++) {
+//             cout << val << " ";
+
+//             val = val * (i - j) / (j + 1);
+//         }
+
+//         cout << endl;
+//     }
+
+//     return 0;
+// }
+
+
+
+#include<iostream>
+#include<vector>
+using namespace std;
+int main(){
+    int m = 5 ;
+    vector< vector<int> > v;
+    for(int i = 1; i <= m ; i++){
+        vector<int> a(i);
+        v.push_back(a);
+    }
+    for(int i = 0 ; i < m ; i++){
+        for(int j = 0 ; j <= i ; j++){
+            if(j==0 || j==i){
+                v[i][j] = 1 ;
+            }
+            else{
+                v[i][j] = v[i-1][j] + v[i-1][j-1];
+            }
+        }
+    }
+    for(int i = 0 ; i < m ; i++){
+        for(int j = 0 ; j <= i ; j++){
+            cout<<v[i][j]<<" ";
+        }
+        cout<<endl;
+    }
+    
+}
